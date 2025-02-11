92ca49c36ab7619aa8ecbc41684a5c158823e78d
Johanes Peter <johanespeter.jp@gmail.com>
2025-02-11 09:37:16 +0700

    first commit

---
+++ src/app/components/snap-scroll-container.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+interface SnapScrollContainerProps {
+  children: React.ReactNode[];
+}
+
+export default function SnapScrollContainer({ children }: SnapScrollContainerProps) {
+  return (
+    <div className="h-screen w-full overflow-y-scroll snap-y snap-mandatory scrollbar-hide">
+      {children.map((child, index) => (
+        <div key={index} className="h-screen w-full snap-center flex items-center justify-center">
+          {child}
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import SnapScrollContainer from "./components/snap-scroll-container";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="no-scrollbar bg-black scrollbar-hide">
+        <SnapScrollContainer>
+          <div className="h-full w-full text-white bg-slate-600">
+            <div>content </div>
+          </div>
+          <div className="h-full w-full text-white bg-red-950">
+            <div>content 2</div>
+          </div>
+          <div className="h-full w-full text-white bg-slate-600">
+            <div>content 3</div>
+          </div>
+          <div className="h-full w-full text-white bg-red-950">
+            <div>content 4</div>
+          </div>
+        </SnapScrollContainer>
+      </main>
+    </div>
+  );
+}
