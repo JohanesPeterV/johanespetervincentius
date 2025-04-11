6d2fcf59f19ee6482d8dd5b814a330eb11f7f8ad
JohanesPeterV <johanespeter.jp@gmail.com>
2025-04-11 18:46:31 +0700

    chore: re arrage technologies

--- src/sections/technologies/technologies.ts
+++ src/sections/technologies/technologies.ts
@@ -14,10 +14,23 @@ export const TECHNOLOGIES: Technology[] = [
       { name: 'Angular', link: 'https://angular.io/docs' },
       { name: 'React', link: 'https://reactjs.org/docs/getting-started.html' },
       {
+        name: 'React Native',
+        link: 'https://reactnative.dev/docs/getting-started',
+      },
+
+      {
+        name: 'Flutter',
+        link: 'https://flutter.dev/?gclid=Cj0KCQjwtrSLBhCLARIsACh6RmjtHujNS1AIhGk-GQXSfuf8gq-KUuiP0CIjFJEBhg0Q5ZjyIybAew8aAmXMEALw_wcB&gclsrc=aw.ds',
+      },
+      {
         name: 'Laravel',
         link: 'https://laravel.com/docs/8.x/installation#meet-laravel',
       },
-      { name: 'Semantic UI React', link: 'https://react.semantic-ui.com/' },
+
+      { name: 'Vue.js', link: 'https://vuejs.org/' },
+      { name: 'Nuxt.js', link: 'https://nuxtjs.org/' },
+      { name: 'Gatsby', link: 'https://www.gatsbyjs.com/' },
+      { name: 'shadcn/ui', link: 'https://ui.shadcn.com/' },
       {
         name: 'ASP.NET',
         link: 'https://dotnet.microsoft.com/en-us/apps/aspnet',
@@ -26,23 +39,18 @@ export const TECHNOLOGIES: Technology[] = [
         name: 'Blazor',
         link: 'https://dotnet.microsoft.com/en-us/apps/aspnet/web-apps/blazor',
       },
-      { name: 'Vue.js', link: 'https://vuejs.org/' },
-      { name: 'Nuxt.js', link: 'https://nuxtjs.org/' },
-      { name: 'Bootstrap', link: 'https://getbootstrap.com/' },
-      { name: 'Tailwind CSS', link: 'https://tailwindcss.com/' },
-      { name: 'Material UI', link: 'https://mui.com/' },
-      { name: 'shadcn/ui', link: 'https://ui.shadcn.com/' },
-      {
-        name: 'Android',
-        link: 'https://developer.android.com/',
-      },
-      {
-        name: 'Flutter',
-        link: 'https://flutter.dev/?gclid=Cj0KCQjwtrSLBhCLARIsACh6RmjtHujNS1AIhGk-GQXSfuf8gq-KUuiP0CIjFJEBhg0Q5ZjyIybAew8aAmXMEALw_wcB&gclsrc=aw.ds',
-      },
+
       {
         name: 'Three JS',
         link: 'https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene',
+      },
+      { name: 'Semantic UI React', link: 'https://react.semantic-ui.com/' },
+      { name: 'Bootstrap', link: 'https://getbootstrap.com/' },
+      { name: 'Tailwind CSS', link: 'https://tailwindcss.com/' },
+      { name: 'Material UI', link: 'https://mui.com/' },
+      {
+        name: 'Android',
+        link: 'https://developer.android.com/',
       },
     ],
   },
@@ -55,6 +63,8 @@ export const TECHNOLOGIES: Technology[] = [
       },
       { name: 'Node.js', link: 'https://nodejs.org/en/' },
       { name: 'Nest.js', link: 'https://nestjs.com/' },
+      { name: 'Express', link: 'https://expressjs.com/' },
+      { name: 'Ruby on Rails', link: 'https://rubyonrails.org/' },
       { name: 'Laravel', link: 'https://laravel.com/' },
       {
         name: 'ASP.NET',
@@ -72,23 +82,23 @@ export const TECHNOLOGIES: Technology[] = [
         link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
       },
       {
-        name: 'C/C++',
-        link: 'https://en.wikipedia.org/wiki/C%2B%2B',
-      },
-      {
         name: 'C#',
         link: 'https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/',
       },
-      { name: 'R', link: 'https://www.r-project.org/' },
+      { name: 'Python', link: 'https://docs.python.org/3/' },
       { name: 'Java', link: 'https://docs.oracle.com/en/java/' },
       { name: 'Dart', link: 'https://dart.dev/guides' },
       {
         name: 'Kotlin',
         link: 'https://developer.android.com/kotlin?gclid=Cj0KCQjwtrSLBhCLARIsACh6RmhUfKr2tiXSAp33Ka688qBDNF1CIEs53jGAXBkiMzvxFOsQg8of-FQaAv8eEALw_wcB&gclsrc=aw.ds',
       },
-      { name: 'Python', link: 'https://docs.python.org/3/' },
       { name: 'PHP', link: 'https://www.php.net/docs.php' },
       { name: 'Go', link: 'https://golang.org/doc/' },
+      {
+        name: 'C/C++',
+        link: 'https://en.wikipedia.org/wiki/C%2B%2B',
+      },
+      { name: 'R', link: 'https://www.r-project.org/' },
     ],
   },
   {
@@ -98,6 +108,7 @@ export const TECHNOLOGIES: Technology[] = [
         name: 'SQL Server',
         link: 'https://docs.microsoft.com/en-us/sql/sql-server/?view=sql-server-ver15',
       },
+      { name: 'PostgreSQL', link: 'https://www.postgresql.org/' },
       { name: 'MongoDB', link: 'https://docs.mongodb.com/' },
       {
         name: 'Cloud Firestore',
@@ -105,13 +116,20 @@ export const TECHNOLOGIES: Technology[] = [
       },
       { name: 'MySQL', link: 'https://dev.mysql.com/doc/' },
       { name: 'SQLite', link: 'https://www.sqlite.org/index.html' },
-      { name: 'PostgreSQL', link: 'https://www.postgresql.org/' },
     ],
   },
 
   {
     category: 'Cloud',
     contents: [
+      {
+        name: 'AWS',
+        link: 'https://aws.amazon.com',
+      },
+      {
+        name: 'Vercel',
+        link: 'https://vercel.com/',
+      },
       {
         name: 'Heroku',
         link: 'https://www.heroku.com/',
