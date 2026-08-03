7382cf97926fb7d49fe7328d1d0995d3124df390
Johanes Peter Vincentius <60497959+JohanesPeterV@users.noreply.github.com>
2026-08-03 23:11:30 +0700

    feat: use Jopditto as site icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,6 +98,12 @@ const configureMetadata = (): Metadata => ({
       'max-snippet': -1,
     },
   },
+  icons: {
+    icon: {
+      url: '/jopditto.webp',
+      type: 'image/webp',
+    },
+  },
   manifest: '/manifest.json',
   other: {
     'apple-mobile-web-app-capable': 'yes',
