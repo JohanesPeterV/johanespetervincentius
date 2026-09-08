74f8b91ae52e00b6b31cc2916572379e7f2fee6a
Johanes Peter Vincentius <60497959+JohanesPeterV@users.noreply.github.com>
2026-09-08 14:38:51 +0700

    style: extend the pause between star formations

--- src/components/dive/dive-atmosphere.tsx
+++ src/components/dive/dive-atmosphere.tsx
@@ -54,7 +54,7 @@ type DiveAtmosphereParams = {
   children?: ReactNode;
 };
 
-const FORMATION_HOLD_SCALE = 4;
+const FORMATION_HOLD_SCALE = 5;
 
 export default function DiveAtmosphere({
   reality,
