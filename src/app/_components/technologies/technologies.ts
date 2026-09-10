604a17c09c04996b49c5ba22ecd35a6375a6be72
Johanes Peter Vincentius <60497959+JohanesPeterV@users.noreply.github.com>
2026-09-10 22:06:52 +0700

    feat: curate tech stack to the shipping stack

--- src/app/_components/technologies/technologies.ts
+++ src/app/_components/technologies/technologies.ts
@@ -8,134 +8,43 @@ export type Technology = {
 
 export const TECHNOLOGIES: Technology[] = [
   {
-    category: 'Front End',
+    category: 'Frontend',
     contents: [
       { name: 'Next.js', link: 'https://nextjs.org/' },
-      { name: 'Angular', link: 'https://angular.io/docs' },
-      { name: 'React', link: 'https://reactjs.org/docs/getting-started.html' },
-      {
-        name: 'React Native',
-        link: 'https://reactnative.dev/docs/getting-started',
-      },
-
-      {
-        name: 'Flutter',
-        link: 'https://flutter.dev/',
-      },
-      {
-        name: 'Laravel',
-        link: 'https://laravel.com/docs/8.x/installation#meet-laravel',
-      },
-
-      { name: 'Vue.js', link: 'https://vuejs.org/' },
-      { name: 'Nuxt.js', link: 'https://nuxtjs.org/' },
-      { name: 'Gatsby', link: 'https://www.gatsbyjs.com/' },
-      { name: 'shadcn/ui', link: 'https://ui.shadcn.com/' },
-      {
-        name: 'ASP.NET',
-        link: 'https://dotnet.microsoft.com/en-us/apps/aspnet',
-      },
-      {
-        name: 'Blazor',
-        link: 'https://dotnet.microsoft.com/en-us/apps/aspnet/web-apps/blazor',
-      },
-
-      {
-        name: 'Three JS',
-        link: 'https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene',
-      },
-      { name: 'Semantic UI React', link: 'https://react.semantic-ui.com/' },
-      { name: 'Bootstrap', link: 'https://getbootstrap.com/' },
-      { name: 'Tailwind CSS', link: 'https://tailwindcss.com/' },
-      { name: 'Material UI', link: 'https://mui.com/' },
-      {
-        name: 'Android',
-        link: 'https://developer.android.com/',
-      },
-    ],
-  },
-  {
-    category: 'Back End',
-    contents: [
-      {
-        name: 'Apollo Server',
-        link: 'https://www.apollographql.com/docs/apollo-server/',
-      },
-      { name: 'Node.js', link: 'https://nodejs.org/en/' },
-      { name: 'Nest.js', link: 'https://nestjs.com/' },
-      { name: 'Express', link: 'https://expressjs.com/' },
-      { name: 'Ruby on Rails', link: 'https://rubyonrails.org/' },
-      { name: 'Laravel', link: 'https://laravel.com/' },
-      {
-        name: 'ASP.NET',
-        link: 'https://dotnet.microsoft.com/en-us/apps/aspnet',
-      },
-    ],
-  },
-  {
-    category: 'Programming',
-    contents: [
+      { name: 'React', link: 'https://react.dev/' },
       { name: 'TypeScript', link: 'https://www.typescriptlang.org/' },
-
-      {
-        name: 'JavaScript',
-        link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
-      },
-      {
-        name: 'C#',
-        link: 'https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/',
-      },
-      { name: 'Python', link: 'https://docs.python.org/3/' },
-      { name: 'Java', link: 'https://docs.oracle.com/en/java/' },
-      { name: 'Dart', link: 'https://dart.dev/guides' },
-      {
-        name: 'Kotlin',
-        link: 'https://developer.android.com/kotlin',
-      },
-      { name: 'PHP', link: 'https://www.php.net/docs.php' },
-      { name: 'Go', link: 'https://golang.org/doc/' },
-      {
-        name: 'C/C++',
-        link: 'https://en.wikipedia.org/wiki/C%2B%2B',
-      },
-      { name: 'R', link: 'https://www.r-project.org/' },
+      { name: 'Tailwind CSS', link: 'https://tailwindcss.com/' },
+      { name: 'shadcn/ui', link: 'https://ui.shadcn.com/' },
+      { name: 'Three.js', link: 'https://threejs.org/' },
     ],
   },
   {
-    category: 'Database',
+    category: 'Backend',
     contents: [
-      {
-        name: 'SQL Server',
-        link: 'https://docs.microsoft.com/en-us/sql/sql-server/?view=sql-server-ver15',
-      },
+      { name: 'Convex', link: 'https://www.convex.dev/' },
+      { name: 'tRPC', link: 'https://trpc.io/' },
+      { name: 'Prisma', link: 'https://www.prisma.io/' },
       { name: 'PostgreSQL', link: 'https://www.postgresql.org/' },
-      { name: 'MongoDB', link: 'https://docs.mongodb.com/' },
-      {
-        name: 'Cloud Firestore',
-        link: 'https://firebase.google.com/docs/firestore',
-      },
-      { name: 'MySQL', link: 'https://dev.mysql.com/doc/' },
-      { name: 'SQLite', link: 'https://www.sqlite.org/index.html' },
+      { name: 'Zod', link: 'https://zod.dev/' },
     ],
   },
-
   {
-    category: 'Cloud',
+    category: 'Platform',
     contents: [
-      {
-        name: 'AWS',
-        link: 'https://aws.amazon.com',
-      },
-      {
-        name: 'Vercel',
-        link: 'https://vercel.com/',
-      },
-      {
-        name: 'Heroku',
-        link: 'https://www.heroku.com/',
-      },
-      { name: 'Netlify', link: 'https://www.netlify.com/' },
-      { name: 'Firebase', link: 'https://firebase.google.com/' },
+      { name: 'Vercel', link: 'https://vercel.com/' },
+      { name: 'Clerk', link: 'https://clerk.com/' },
+      { name: 'Stripe', link: 'https://stripe.com/docs' },
+      { name: 'Resend', link: 'https://resend.com/' },
+      { name: 'Upstash Redis', link: 'https://upstash.com/' },
+    ],
+  },
+  {
+    category: 'Tooling',
+    contents: [
+      { name: 'Turborepo', link: 'https://turbo.build/' },
+      { name: 'Playwright', link: 'https://playwright.dev/' },
+      { name: 'Sentry', link: 'https://sentry.io/' },
+      { name: 'PostHog', link: 'https://posthog.com/' },
     ],
   },
 ];
